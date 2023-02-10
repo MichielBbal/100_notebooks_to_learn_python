0721be0e872d1ca3dbd8498fcb26531e0b42acbb
Michiel Bontenbal <37507786+MichielBbal@users.noreply.github.com>
2023-02-10 11:06:05 +0100

    first upload

---
+++ config.py
@@ -0,0 +1,15 @@
+#Mastodon
+mastodon_account = 'michiel.bontenbal@gmail.com'
+mastodon_pw = 'RpDMyvXNJyuhx6t'
+mastodon_client_key = 'oezli5inp_mISOVnBYdVye4nI45nfdZp7xK6xyXosdk'
+mastodon_client_secret = 'Egc5O7-heIusb7deRrbZPzGPtv8lZ-DLvhxWuKRAw7I' 
+mastodon_access_token = 'rxbWelcSEc4F9Mhn2B2mTTwhHIXHGekBObdAygyGX-s'
+
+#Twitter
+
+#OpenEars MQTT
+oe_user = 'michiel'
+oe_pw = '9B12829FC6F041A7'
+
+#mySQL
+mysql_pw = "pw=Thomas2012"
