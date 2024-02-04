ce7c7356c704da69a509bf4ba78f2c015e54e783
Michiel Bontenbal <37507786+MichielBbal@users.noreply.github.com>
2024-02-04 15:00:56 +0100

    4 feb

--- tranquilizer.py
+++
@@ -1,6 +0,0 @@
-from tranquilizer import tranquilize
-
-@tranquilize()
-def order(cheese):
-    '''I'd like to buy some cheese!'''
-    return "I'm afraid we're fresh out of {}, Sir.".format(cheese)
